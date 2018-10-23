008363ee00f499704f49ed0f24b219d0222bb572
Farida <faridah.absi@gmail.com>
2018-10-23 20:17:52 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // Code Goes Here
+
+<MovieBrowser>
+  <Movie title="Mad Max: Fury Road" />
+  <Movie title="Harry Potter & The Goblet Of Fire" />
+</MovieBrowser>
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+ 
+    return (
+      <div className="movie-browser">
+        {this.props.children}
+      </div>      
+    );
+  }
+}
